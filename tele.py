ada1a2943e3a6d691f29b0fcd5408bff59fa209d
Agunivers7 <herokuone7@gmail.com>
2023-02-26 03:48:27 -0800

    Update tele.py

--- tele.py
+++ tele.py
@@ -1,4 +1,4 @@
-Traceback (most recent call last):
-  File "e:\facerego\face.py", line 7, in <module>
-    cv2.imshow("Camera", frame)
-cv2.error: OpenCV(4.7.0) D:\a\opencv-python\opencv-python\opencv\modules\highgui\src\window.cpp:971: error: (-215:Assertion failed) size.width>0 && size.height>0 in function 'cv::imshow'
+ File "e:\facerego\face.py", line 7, in <module>
+    height, width, _ = img.shape
+AttributeError: 'NoneType' object has no attribute 'shape'
+  
